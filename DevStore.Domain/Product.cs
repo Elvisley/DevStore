58e9c60c9287f145562efb4d60c93927a5ecc075
Elvisley Souza <elvis.s.p90@gmail.com>
2016-11-08 17:18:28 -0200

    Iniciando projeto web api c# - Estudo

---
+++ DevStore.Domain/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DevStore.Domain
+{
+    public class Product
+    {
+
+        public Product()
+        {
+            this.AcquiredDate = DateTime.Now; 
+        }
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime AcquiredDate { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public virtual Category category{ get; set; }
+
+        public override string ToString()
+        {
+            return this.Title;
+        }
+
+    }
+}
---
+++ DevStore.Infra/DataContexts/DevStoreDataContext.cs
@@ -0,0 +1,42 @@
+﻿using DevStore.Domain;
+using System.Data.Entity;
+
+namespace DevStore.Infra.DataContexts
+{
+    public class DevStoreDataContext : DbContext
+    {
+        public DevStoreDataContext() : base("devStoreConnectionString")
+        {
+            Database.SetInitializer(new DevStoreDataContextInitializer());
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+
+
+    public class DevStoreDataContextInitializer : DropCreateDatabaseIfModelChanges<DevStoreDataContext>
+    {
+        protected override void Seed(DevStoreDataContext context)
+        {
+            context.Categories.Add(new Category { Id = 1 , Title = "Informática" });
+            context.Categories.Add(new Category { Id = 2, Title = "Games" });
+            context.Categories.Add(new Category { Id = 3, Title = "Papelaria" });
+            context.SaveChanges();
+
+            context.Products.Add(new Product { Id = 1 , CategoryId = 1 , IsActive = true , Title = "Macbook Pro 13" , Price = 5999 });
+            context.Products.Add(new Product { Id = 2, CategoryId = 1, IsActive = true, Title = "Computador Intel Core I5 5ª Geracao ", Price = 1250 });
+            context.Products.Add(new Product { Id = 3, CategoryId = 1, IsActive = true, Title = "Kit mouse e teclado dell ", Price = 159 });
+
+            context.Products.Add(new Product { Id = 4, CategoryId = 2, IsActive = true, Title = "PS4 + 2 Controles + 1 Jogo", Price = 1699 });
+            context.Products.Add(new Product { Id = 5, CategoryId = 2, IsActive = true, Title = "Xbox One 500 Giga" , Price = 1500 });
+            context.Products.Add(new Product { Id = 6, CategoryId = 2, IsActive = true, Title = "Jogo PS4 Metal Slug", Price = 99 });
+
+            context.Products.Add(new Product { Id = 7, CategoryId = 3, IsActive = true, Title = "Caderno Tilibra", Price = 9 });
+
+            base.Seed(context);
+        }
+    }
+}
